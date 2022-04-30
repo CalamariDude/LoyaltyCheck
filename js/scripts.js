8645671923f55b1bc16b6f06b514f654e0fd56fa
jad <zeineddine.jad@gmail.com>
2022-04-30 02:27:30 -0500

    hello

--- js/scripts.js
+++ js/scripts.js
@@ -3,58 +3,6 @@
 * Copyright 2013-2022 Start Bootstrap
 * Licensed under MIT (https://github.com/StartBootstrap/startbootstrap-stylish-portfolio/blob/master/LICENSE)
 */
-window.addEventListener('DOMContentLoaded', event => {
-
-    const sidebarWrapper = document.getElementById('sidebar-wrapper');
-    let scrollToTopVisible = false;
-    // Closes the sidebar menu
-    const menuToggle = document.body.querySelector('.menu-toggle');
-    menuToggle.addEventListener('click', event => {
-        event.preventDefault();
-        sidebarWrapper.classList.toggle('active');
-        _toggleMenuIcon();
-        menuToggle.classList.toggle('active');
-    })
-
-    // Closes responsive menu when a scroll trigger link is clicked
-    var scrollTriggerList = [].slice.call(document.querySelectorAll('#sidebar-wrapper .js-scroll-trigger'));
-    scrollTriggerList.map(scrollTrigger => {
-        scrollTrigger.addEventListener('click', () => {
-            sidebarWrapper.classList.remove('active');
-            menuToggle.classList.remove('active');
-            _toggleMenuIcon();
-        })
-    });
-
-    function _toggleMenuIcon() {
-        const menuToggleBars = document.body.querySelector('.menu-toggle > .fa-bars');
-        const menuToggleTimes = document.body.querySelector('.menu-toggle > .fa-xmark');
-        if (menuToggleBars) {
-            menuToggleBars.classList.remove('fa-bars');
-            menuToggleBars.classList.add('fa-xmark');
-        }
-        if (menuToggleTimes) {
-            menuToggleTimes.classList.remove('fa-xmark');
-            menuToggleTimes.classList.add('fa-bars');
-        }
-    }
-
-    // Scroll to top button appear
-    document.addEventListener('scroll', () => {
-        const scrollToTop = document.body.querySelector('.scroll-to-top');
-        if (document.documentElement.scrollTop > 100) {
-            if (!scrollToTopVisible) {
-                fadeIn(scrollToTop);
-                scrollToTopVisible = true;
-            }
-        } else {
-            if (scrollToTopVisible) {
-                fadeOut(scrollToTop);
-                scrollToTopVisible = false;
-            }
-        }
-    })
-})
 
 function fadeOut(el) {
     el.style.opacity = 1;
@@ -86,3 +34,19 @@ function find_out() {
     window.location = `form.html?ig=${ig}`
 };
 
+function changeCss () {
+    if (this.scrollY >786){
+        for (left_item in document.querySelectorAll(".fly-in-left")){
+            console.log(left_item)
+            var amount_left = (left_item.getBoundingClientRect.top-786)/742 * 360;
+            left_item.style = `transform: translateX(-${amount_left}px);`;
+        }
+        for ( right_item in document.querySelectorAll(".fly-in-right")){
+            var amount_right = (right_item.getBoundingClientRect.top-786)/742 * 360;
+            right_item.style = `transform: translateX(${amount_right}px);`;
+        }
+    }
+/* this.scrollY > 500 ? chrono_item.style.opacity = .8 : chrono_item.style.opacity = 1; */
+  }
+  
+window.addEventListener("scroll", changeCss , false);
